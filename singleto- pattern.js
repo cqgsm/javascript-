e38286096e8f1da2b9a162b3bf1da4429101d5cc
cqgsm <260422909@qq.com>
2015-12-03 02:05:02 +0800

    Update singleto- pattern.js
    第一个模式学习，单例，适用于我们只需要一个对象的时候，例如window对象。

--- singleto- pattern.js
+++ singleto- pattern.js
@@ -2,7 +2,7 @@
 // first-day javascript单例模式
 //单例模式：保证一个类只有一个实例，并提供一个访问他的全局访问点
 
-    // 1.最简单的写法（面向对象写法）
+// 1.最简单的写法（面向对象写法）
     var Singleton = function (name) {
         this.name = name;
         this.instance = null;
@@ -21,7 +21,7 @@
     var bb = Singleton.getInstance('guosimeng');
     console.log(aa === bb);//true
 
-    //2.闭包版本
+//2.闭包版本
     var Singleton = function (name) {
         this.name = name;
     }
